00c3d53271e9052af4bf955b269ddca62af68ef3
Vianney Tran <vianney.tran@datadoghq.com>
2018-03-14 11:56:14 +0100

    [tests][errors] Make TestFindIsDstSizeTooSmallError stricter
    TestFindIsDstSizeTooSmallError now check that there is ONE AND ONLY ONE zstd error that corresponds to DstSizeTooSmall error to make sure the check is not too leniant (before just tested we had at least one)

--- errors_test.go
+++ errors_test.go
@@ -13,12 +13,17 @@ const (
 // TestFindIsDstSizeTooSmallError tests that there is at least one error code that
 // corresponds to dst size too small
 func TestFindIsDstSizeTooSmallError(t *testing.T) {
+	found := 0
 	for i := -1; i > -ErrorUpperBound; i-- {
 		e := ErrorCode(i)
 		if IsDstSizeTooSmallError(e) {
-			return // Found
+			found++
 		}
 	}
 
-	t.Fatal("Couldn't find an error code for DstSizeTooSmall error, please make sure we didn't change the error string")
+	if found == 0 {
+		t.Fatal("Couldn't find an error code for DstSizeTooSmall error, please make sure we didn't change the error string")
+	} else if found > 1 {
+		t.Fatal("IsDstSizeTooSmallError found multiple error codes matching, this shouldn't be the case")
+	}
 }
