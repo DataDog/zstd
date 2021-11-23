0c4231e2c451a9233062cfe376c3ab09db1daca0
Francois Visconte <francois.visconte@datadoghq.com>
2021-11-23 11:48:45 +0100

    Add version constraint for building against external library

--- external_zstd.go
+++ external_zstd.go
@@ -5,4 +5,10 @@ package zstd
 
 // #cgo CFLAGS: -DUSE_EXTERNAL_ZSTD
 // #cgo pkg-config: libzstd
+/*
+#include<zstd.h>
+#if ZSTD_VERSION_NUMBER < 10400
+#error "ZSTD version >= 1.4 is required"
+#endif
+*/
 import "C"
